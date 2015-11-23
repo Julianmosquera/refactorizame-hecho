a3fc255001eadcaf87530b4759ea90c63cb9148b
mmoureperez <mmoureperez@atanasoff02.danielcastelao.org>
2015-11-23 10:00:26 +0100

    cambios3

--- src/refactorizar/Refactorizar.java
+++ src/refactorizar/Refactorizar.java
@@ -1,5 +1,7 @@
 package refactorizar;
 
+import java.util.Scanner;
+
 /**
  * Lista los numeros primos de dos cifras
  *
@@ -10,8 +12,9 @@ public class Refactorizar {
         boolean esPrimo = false;// p=esPrimo.
         int ndigitos=0 ;
         int numDigitos=0 ;
-        numDigitos = 4; //introducir un metodo en estas lineas que nos retorne numdigitos.
-        introduceDigitos(numDigitos);
+        numDigitos = introduceDigitos(); //introducir un metodo en estas lineas que nos retorne numdigitos.
+        
+        
         for (int numComparar = 1; numComparar <= 99999; numComparar++) {    
             ndigitos = contadorDivisionEntera(numComparar);
 
@@ -29,19 +32,7 @@ public class Refactorizar {
                             limite--;
                         }
 
-                        while (i1 <= limite) {//otros metodo calculaNumDivisores.
-                            if (numComparar % i1 == 0) {
-                                contadorDivisores++;
-                            }
-                            i1 += 2;
-                            if (contadorDivisores == 2) {
-                                i1 = limite + 1;
-                            }
-                        }
-
-                        if (contadorDivisores == 1) {//si solo se divide por el mismo.
-                            esPrimo = true;
-                        }
+                        esPrimo = calculaNumDivisores(i1, limite, numComparar, contadorDivisores);
                     }
                 }
 
@@ -50,6 +41,23 @@ public class Refactorizar {
                 }
             }
         }
+    }
+
+    public static boolean calculaNumDivisores(int i1, int limite, int numComparar, int contadorDivisores) {
+        boolean esPrimo = false;
+        while (i1 <= limite) {//otros metodo calculaNumDivisores.
+            if (numComparar % i1 == 0) {
+                contadorDivisores++;
+            }
+            i1 += 2;
+            if (contadorDivisores == 2) {
+                i1 = limite + 1;
+            }
+        }
+        if (contadorDivisores == 1) {//si solo se divide por el mismo.
+            esPrimo = true;
+        }
+        return esPrimo;
     }
 
     public static int contadorDivisionEntera(int numComparar) {
@@ -65,10 +73,14 @@ public class Refactorizar {
         return ndigitos;
     }
 
-    public static void introduceDigitos(int numDigitos) {
+    public static int introduceDigitos() {
+        Scanner obx= new Scanner(System.in);
+        System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
+        int numDigitos=obx.nextInt();
         if (numDigitos <= 0) {
             System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
         }
+        return numDigitos;
     }
 
 }
